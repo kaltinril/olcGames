d181a4df35336560d30b681b2fd982c47a07df46
Jeremy Swartwood <jeremy.swartwood@gmail.com>
2020-04-25 16:05:47 -0800

    Working snow fall, accumulation, and spawning with mouse click.

--- snow/snow/main.cpp
+++ snow/snow/main.cpp
@@ -12,36 +12,78 @@ public:
 	}
 
 private:
-	int flakes[256][240] = { 0 };
+	int flakes[240][256] = { 0 };
+	int frame_skip = 10;
+	int frame = 0;
 
-	void SpawnSnow() {
-		for (int i = 0; i < 100; i++)
+	void SpawnSnow(int number_of_flakes) {
+		for (int i = 0; i < number_of_flakes; i++)
 		{
 			int color = 128 + rand() % 128;
 			int x = rand() % 256;
-			int y = rand() % 5;
-			flakes[x][y] = color;
+			int y = rand() % 2;
+			flakes[y][x] = color;
 		}
+	}
+
+	void MoveSnow()
+	{
+		int direction = (rand() % 3) - 1;
+
+		for (int y = ScreenHeight() - 2; y >= 0; y--) {
+			for (int x = 0; x < ScreenWidth(); x++)
+			{
+
+				direction = (rand() % 3) - 1;
+				if (flakes[y + 1][x + direction] == 0 && flakes[y][x] != 0) {
+					if (y > ScreenHeight() - 6)
+						y = y;
+
+					flakes[y + 1][x + direction] = flakes[y][x];
+					flakes[y][x] = 0;
+				}
+
+			}
+		}
+	}
+
+	void SpawnSnowAtSpot(int x_in, int y_in) {
+		int color = 128 + rand() % 128;
+		flakes[y_in][x_in] = color;
 	}
 
 public:
 	bool OnUserCreate() override
 	{
 		// Create some random snow flakes
-		SpawnSnow();
+		SpawnSnow(5);
 
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
+		// Spawn new snow
+		if (GetMouse(0).bHeld)
+		{
+			SpawnSnowAtSpot(GetMouseX(), GetMouseY());
+		}
+
+
 		// draw snow
 		for (int x = 0; x < ScreenWidth(); x++)
 			for (int y = 0; y < ScreenHeight(); y++)
-				Draw(x, y, olc::Pixel(flakes[x][y], flakes[x][y], flakes[x][y]));
+				Draw(x, y, olc::Pixel(flakes[y][x], flakes[y][x], flakes[y][x]));
 
 		// Move snow down
-
+		frame++;
+		if (frame > frame_skip)
+		{
+			SpawnSnow(1);
+			MoveSnow();
+			frame = 0;
+		}
+		
 				
 		return true;
 	}
