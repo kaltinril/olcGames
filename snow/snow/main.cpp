ad4a496c5fc58ce8c31ed914e7071b2477d046bf
Jeremy Swartwood <jeremy.swartwood@gmail.com>
2020-04-25 17:44:13 -0800

    Add ability to 'shovel' snow away.  Add R reset button.

--- snow/snow/main.cpp
+++ snow/snow/main.cpp
@@ -24,7 +24,7 @@ private:
 		int y = 0;
 		for (int i = 0; i < number_of_flakes; i++)
 		{
-			color = 128 + rand() % 128;
+			color = 126 + rand() % 128;
 			x = rand() % 256;
 			y = rand() % 2;
 			// Only place it if there is room, if the screen fills up we don't wnat to randomly change the pixel color
@@ -35,7 +35,7 @@ private:
 
 	void MoveSnow()
 	{
-		int direction = (rand() % 3) - 1;
+		int direction = (rand() % 5) - 2;
 		for (int y = ScreenHeight() - 3; y >= 0; y--) {
 			for (int x = 0; x < ScreenWidth(); x++)
 			{
@@ -48,8 +48,31 @@ private:
 		}
 	}
 
+	// Set the snow flakes array position to a random greyscale color from 128-255
 	void SpawnSnowAtSpot(int x_in, int y_in) {
-		flakes[y_in][x_in] = 128 + rand() % 128;
+		flakes[y_in][x_in] = 126 + rand() % 128;
+	}
+
+	// Remove a 5x5 square
+	void RemoveSnow(int x_in, int y_in) {
+		int ball_size = 3;
+		int maxX = std::clamp(x_in + ball_size, 0, ScreenWidth());
+		int maxY = std::clamp(y_in + ball_size, 0, ScreenHeight());
+		int minX = std::clamp(x_in - ball_size, 0, ScreenWidth());
+		int minY = std::clamp(y_in - ball_size, 0, ScreenHeight());
+
+		for (int x = minX; x < maxX; x++)
+			for (int y = minY; y < maxY; y++)
+				flakes[y][x] = 0;
+	}
+
+	// Reset all user changable values and the screen/board
+	void ResetBoard() {
+		for (int x = 0; x < ScreenWidth(); x++)
+			for (int y = 0; y < ScreenHeight(); y++)
+				flakes[y][x] = 0;
+		
+		snow_fall_rate = 0;
 	}
 
 public:
@@ -65,6 +88,10 @@ public:
 		// Spawn new snow
 		if (GetMouse(0).bHeld)
 			SpawnSnowAtSpot(GetMouseX(), GetMouseY());
+		
+		// Remove snow at spot
+		if (GetMouse(1).bHeld)
+			RemoveSnow(GetMouseX(), GetMouseY());
 
 		// Increase snow fall rate if UP arrow pressed
 		if (GetKey(olc::Key::UP).bPressed) {
@@ -79,6 +106,10 @@ public:
 			if (snow_fall_rate < 0)
 				snow_fall_rate = 0;
 		}
+
+		// [R] Reset the board
+		if (GetKey(olc::Key::R).bPressed)
+			ResetBoard();
 
 		// draw snow
 		for (int x = 0; x < ScreenWidth(); x++)
