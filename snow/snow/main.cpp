36a1a2c97c397b43a49b6ce6e884f5c18ca6568c
Jeremy Swartwood <jeremy.swartwood@gmail.com>
2020-04-25 16:46:37 -0800

    Cleanup new lines

--- snow/snow/main.cpp
+++ snow/snow/main.cpp
@@ -7,27 +7,26 @@ class Snow : public olc::PixelGameEngine
 public:
 	Snow()
 	{
-		// Name you application
 		sAppName = "Snow";
 	}
 
 private:
 	int flakes[240][256] = { 0 };
-
 	int snow_fall_rate = 0;
 	int max_snow_fall_rate = 50;
-
 	float animate_elapsed = 0.0f;
 	float animate_rate = 0.03125f;  // 1.0f is 1 second.  0.03125f is  1/32 of a second
 
 
 	void SpawnSnow(int number_of_flakes) {
+		int color = 0;
+		int x = 0;
+		int y = 0;
 		for (int i = 0; i < number_of_flakes; i++)
 		{
-			int color = 128 + rand() % 128;
-			int x = rand() % 256;
-			int y = rand() % 2;
-
+			color = 128 + rand() % 128;
+			x = rand() % 256;
+			y = rand() % 2;
 			// Only place it if there is room, if the screen fills up we don't wnat to randomly change the pixel color
 			if (flakes[y][x] == 0)
 				flakes[y][x] = color;
@@ -37,11 +36,9 @@ private:
 	void MoveSnow()
 	{
 		int direction = (rand() % 3) - 1;
-
 		for (int y = ScreenHeight() - 2; y >= 0; y--) {
 			for (int x = 0; x < ScreenWidth(); x++)
 			{
-
 				direction = (rand() % 3) - 1;
 				if (flakes[y + 1][x + direction] == 0 && flakes[y][x] != 0) {
 					flakes[y + 1][x + direction] = flakes[y][x];
@@ -52,8 +49,7 @@ private:
 	}
 
 	void SpawnSnowAtSpot(int x_in, int y_in) {
-		int color = 128 + rand() % 128;
-		flakes[y_in][x_in] = color;
+		flakes[y_in][x_in] = 128 + rand() % 128;
 	}
 
 public:
