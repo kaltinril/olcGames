d8fa9528297d13cf6b9d1e25999a7eeb76651162
Jeremy Swartwood <jeremy.swartwood@gmail.com>
2020-04-25 16:35:46 -0800

    Cleanup.  Change frame delay to elapsed time.

--- snow/snow/main.cpp
+++ snow/snow/main.cpp
@@ -13,11 +13,13 @@ public:
 
 private:
 	int flakes[240][256] = { 0 };
-	int frame_skip = 10;
-	int frame = 0;
 
 	int snow_fall_rate = 0;
 	int max_snow_fall_rate = 50;
+
+	float animate_elapsed = 0.0f;
+	float animate_rate = 0.03125f;  // 1.0f is 1 second.  0.03125f is  1/32 of a second
+
 
 	void SpawnSnow(int number_of_flakes) {
 		for (int i = 0; i < number_of_flakes; i++)
@@ -42,13 +44,9 @@ private:
 
 				direction = (rand() % 3) - 1;
 				if (flakes[y + 1][x + direction] == 0 && flakes[y][x] != 0) {
-					if (y > ScreenHeight() - 6)
-						y = y;
-
 					flakes[y + 1][x + direction] = flakes[y][x];
 					flakes[y][x] = 0;
 				}
-
 			}
 		}
 	}
@@ -61,9 +59,8 @@ private:
 public:
 	bool OnUserCreate() override
 	{
-		// Create some random snow flakes
-		SpawnSnow(100);
-
+		// Create some random snow flakes to begin with
+		SpawnSnow(25);
 		return true;
 	}
 
@@ -73,33 +70,33 @@ public:
 		if (GetMouse(0).bHeld)
 			SpawnSnowAtSpot(GetMouseX(), GetMouseY());
 
+		// Increase snow fall rate if UP arrow pressed
 		if (GetKey(olc::Key::UP).bPressed) {
 			snow_fall_rate++;
 			if (snow_fall_rate > max_snow_fall_rate)
 				snow_fall_rate = max_snow_fall_rate;
 		}
 
+		// Decrease snow fall rate if DOWN arrow pressed
 		if (GetKey(olc::Key::DOWN).bPressed) {
 			snow_fall_rate--;
 			if (snow_fall_rate < 0)
 				snow_fall_rate = 0;
 		}
 
-
 		// draw snow
 		for (int x = 0; x < ScreenWidth(); x++)
 			for (int y = 0; y < ScreenHeight(); y++)
 				Draw(x, y, olc::Pixel(flakes[y][x], flakes[y][x], flakes[y][x]));
 
-		// Move snow down
-		frame++;
-		if (frame > frame_skip)
-		{
+		// Move snow down, using elasped time for consitent behavior on faster hardware
+		animate_elapsed += fElapsedTime;
+		if (animate_elapsed > animate_rate) {
+			animate_elapsed = animate_elapsed - animate_rate;
 			SpawnSnow(snow_fall_rate); // Add some snowfall randomly
 			MoveSnow();
-			frame = 0;
 		}
-		
+
 		return true;
 	}
 };
