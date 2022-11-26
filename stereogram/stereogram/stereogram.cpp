86587718d09bbff6b310984376f73d4e5e19afec
Jeremy Swartwood <jeremy.swartwood@gmail.com>
2022-11-25 21:56:05 -0900

    Fix issue where I didn't understand the cycling/tiling/recursive part

--- stereogram/stereogram/stereogram.cpp
+++ stereogram/stereogram/stereogram.cpp
@@ -8,13 +8,17 @@ public:
 
 	StereoGram()
 	{
-		sAppName = "Example";
+		sAppName = "Autostereogram";
 	}
 
-	void create_before_pattern()
+	/// <summary>
+	///  The dot pattern needs to be created once on the left.  These pixels should not be touched and should not contain mask image data to adjust.
+	/// </summary>
+	/// <param name="pattern_repetitions">The number of "Sections" of the screen.  Default 10</param>
+	void create_dot_pattern(int pattern_repetitions = 10)
 	{
 		// create the LEFT pattern and draw it also at the RIGHT side
-		for (int x = 0; x < ScreenWidth() / 2; x++)
+		for (int x = 0; x < ScreenWidth() / pattern_repetitions; x++)
 			for (int y = 0; y < ScreenHeight(); y++)
 			{
 				int color = (rand() % 2) * 255;
@@ -23,9 +27,46 @@ public:
 				int blue = color;//(rand() % 2) * 255;
 				olc::Pixel pix = olc::Pixel(red, green, blue);
 				Draw(x, y, pix);
-				Draw(x + ScreenWidth() / 2, y, pix);
 			}
 	}
+
+	/// <summary>
+	/// Assumption that the threeDImage loaded image is the same size as the screen.
+	/// </summary>
+	/// <param name="pattern_repetitions"></param>
+	void add_stereogram_sprite(int pattern_repetitions = 10, int inverse = 1)
+	{
+		// Eventually take in a sprite, convert it to greyscale, use the greyscale to change the "offset" or shift of the pixels
+		bool debug = false;
+		int imgX = 0;
+		int imgY = 0;
+		int offset = 0;
+		int pattern_width = ScreenWidth() / pattern_repetitions;
+		olc::Pixel pix;
+		olc::Pixel imagePix;
+		// Loop over the remainder of the screen
+		// Each "pattern width" wide section is then re-used to sample the data we need for the next "pattern width" section
+		// This is a recursive/itterative approach
+		for (int y = 0; y < ScreenHeight(); y++)
+		{
+			for (int x = pattern_width; x < ScreenWidth(); x++)
+			{
+				imagePix = threeDImage->GetPixel(x, y);
+				int greyscale = (imagePix.r + imagePix.g + imagePix.b) / 3; // Just in case someone didn't use greyscale for the image, convert it to greysacale
+				offset = greyscale / (255 / pattern_repetitions);
+
+				int samplePixelPosition = (x - pattern_width);
+				int shiftedSamplePosition = samplePixelPosition + (offset * inverse);
+				pix = PixelGameEngine::GetDrawTarget()->GetPixel(shiftedSamplePosition, y);
+
+				if (debug == true)
+					pix = olc::Pixel(255, 0, 0);
+
+				Draw(x, y, pix);
+			}
+		}
+	}
+
 
 	void add_stereogram_square()
 	{
@@ -40,48 +81,13 @@ public:
 				olc::Pixel pix = PixelGameEngine::GetDrawTarget()->GetPixel(x, y);
 
 				if (debug == true)
-					if (x == startx || x == endx - 1 || y == starty || y == endy -1)
+					if (x == startx || x == endx - 1 || y == starty || y == endy - 1)
 						pix = olc::Pixel(255, 0, 0);
 
 				Draw(x - 3, y, pix);
 			}
 	}
 
-	void add_stereogram_sprite()
-	{
-		// Eventually take in a sprite, convert it to greyscale, use the greyscale to change the "offset" or shift of the pixels
-		bool debug = false;
-		int imgX = 0;
-		int imgY = 0;
-		int offset = 0;
-		int topLeftDrawX = (3*ScreenWidth() / 4) - (threeDImage->width / 2);
-		int topLeftDrawY = (ScreenHeight() / 2) - (threeDImage->height / 2);
-		olc::Pixel pix;
-		olc::Pixel imagePix;
-		for (int imgY = 0; imgY < threeDImage->height; imgY++)
-		{
-			for (int imgX = 0; imgX < threeDImage->width; imgX++)
-			{
-				imagePix = threeDImage->GetPixel(imgX, imgY);
-				int greyscale = (imagePix.r + imagePix.g + imagePix.b) / 3;
-
-				if (greyscale > 0)
-				{
-					offset = greyscale / (255 / 5);
-
-
-					int x = imgX + topLeftDrawX;
-					int y = imgY + topLeftDrawY;
-					pix = PixelGameEngine::GetDrawTarget()->GetPixel(x, y);
-
-					if (debug == true)
-						pix = olc::Pixel(255, 0, 0);
-					
-					Draw(x - offset, y, pix);
-				}
-			}
-		}
-	}
 
 	void stamp_background()
 	{
@@ -113,16 +119,13 @@ public:
 	{
 		
 		// Load the sprite
-		threeDImage = std::make_unique<olc::Sprite>("./star_shaded.png");
+		threeDImage = std::make_unique<olc::Sprite>("./800x500.png");
 		backgroundPattern = std::make_unique<olc::Sprite>("./background_2.png");
 
-		// Called once at the start, so create things here
-		create_before_pattern();
+		
+		create_dot_pattern(10);
 		//stamp_background();
-		add_stereogram_sprite();
-		//add_stereogram_sprite();
-		//add_stereogram_sprite2();
-		//add_stereogram_sprite3();
+		add_stereogram_sprite(10, 1);
 		return true;
 	}
 
@@ -137,7 +140,7 @@ public:
 int main()
 {
 	StereoGram demo;
-	if (demo.Construct(300, 200, 2, 2, false, false))
+	if (demo.Construct(800, 500, 2, 2, false, false))
 		demo.Start();
 
 	return 0;
